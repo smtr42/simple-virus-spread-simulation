325fb996ab4a72dee005955e97408839ee2b9362
smtr42 <simteiva@yahoo.fr>
2020-03-18 20:28:23 +0100

    Added track of time

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import pygame
+
 import particle
 
 pygame.display.set_caption('Tutorial 10')
-wh = (800, 600)
-screen = pygame.display.set_mode(wh)
-env = particle.Environment(wh)
+screen_dimensions = (800, 600)
+screen = pygame.display.set_mode(screen_dimensions)
+sim = particle.Simulation(screen_dimensions)
 
-env.addParticles(15)
-env.addParticles(x=200, y=250, size=10, speed=1, angle=0)
+sim.add_particle(100)
+sim.add_particle(x=200, y=250, size=10, speed=1, angle=0)
 timer = pygame.time.Clock()
+elapsed_time = 0
 
 selected_particle = None
 running = True
@@ -16,20 +18,14 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    #     elif event.type == pygame.MOUSEBUTTONDOWN:
-    #         selected_particle = env.findParticle(pygame.mouse.get_pos())
-    #     elif event.type == pygame.MOUSEBUTTONUP:
-    #         selected_particle = None
-    #
-    # if selected_particle:
-    #     selected_particle.mouseMove(pygame.mouse.get_pos())
 
     dt = timer.tick() / 1000
+    elapsed_time += dt
+    sim.update(elapsed_time, dt)
+    screen.fill(sim.colour)
 
-    env.update()
-    screen.fill(env.colour)
-
-    for p in env.particles:
-        pygame.draw.circle(screen, p.colour, (int(p.x), int(p.y)), p.size, p.thickness)
+    for p in sim.particles:
+        pygame.draw.circle(screen, p.colour, (int(p.x), int(p.y)), p.size,
+                           p.thickness)
 
     pygame.display.flip()
