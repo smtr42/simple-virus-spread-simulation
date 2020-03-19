163ead20354db5983dc3104cad068a95fc987da1
smtr42 <simteiva@yahoo.fr>
2020-03-19 10:40:02 +0100

    added chart representation

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import pygame
-
+from graph import Grapher
 import particle
+import os
+x = 100
+y = 100
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (x,y)
 
 pygame.display.set_caption('Tutorial 10')
-screen_dimensions = (800, 600)
+screen_dimensions = (800, 400)
 screen = pygame.display.set_mode(screen_dimensions)
-sim = particle.Simulation(screen_dimensions)
+pygame.display.set_caption("Covid spread simulation")
 
-# sim.add_particle(10)
-sim.add_particle(n=150, speed=6, freezed=90, killer=3)
+sim = particle.Simulation(screen_dimensions)
+graph = Grapher()
+
+sim.add_particle(n=150, speed=6, freezed=93, killer=4)
+sim.add_wall(add=False)
+
 timer = pygame.time.Clock()
 elapsed_time = 0
+time_serie=[]
 
 selected_particle = None
 running = True
@@ -27,4 +36,14 @@ while running:
         pygame.draw.circle(screen, p.colour, (int(p.x), int(p.y)), p.size,
                            p.thickness)
 
+    if isinstance(sim.wall, particle.Wall):
+        pygame.draw.rect(screen, sim.wall.colour, (
+        sim.wall.x, sim.wall.y, sim.wall.width, sim.wall.height),
+                         sim.wall.thickness)
     pygame.display.flip()
+    elapsed_time += dt
+    time_serie.append(elapsed_time)
+
+    # graph.plot_healthy(time_serie, sim.healthy)
+    # graph.plot_sick(time_serie, sim.sick)
+    # graph.show()
